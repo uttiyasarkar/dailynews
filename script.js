0d5b5b03aa3ce36d5144c4cbd191f81aac56b19b
uttiya <uttiyasarkar@gmail.com>
2023-06-13 11:32:18 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".toggle").on("click", function() {
+    $(".toggle").parent().toggleClass('active');
+    
+   
+  });
+  
+  
+  // $(".navbar a").on("click" ,function(){
+  //    $( ".container" ).slideUp( "slow" );
+  // });
+  
